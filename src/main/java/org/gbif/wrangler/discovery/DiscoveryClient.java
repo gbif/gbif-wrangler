d39788ba6570679dd63d961204a51b955428b107
fmendezh <federicomh@gmail.com>
2024-05-15 17:32:09 +0200

    formatting

--- src/main/java/org/gbif/wrangler/discovery/DiscoveryClient.java
+++ src/main/java/org/gbif/wrangler/discovery/DiscoveryClient.java
@@ -1,8 +1,23 @@
+/*
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
 package org.gbif.wrangler.discovery;
 
-import lombok.Builder;
-import lombok.Data;
-import lombok.SneakyThrows;
+import java.io.Closeable;
+import java.io.IOException;
+import java.net.InetAddress;
+import java.util.Optional;
+
 import org.apache.curator.RetryPolicy;
 import org.apache.curator.framework.CuratorFramework;
 import org.apache.curator.framework.CuratorFrameworkFactory;
@@ -11,10 +26,9 @@ import org.apache.curator.x.discovery.ServiceDiscovery;
 import org.apache.curator.x.discovery.ServiceDiscoveryBuilder;
 import org.apache.curator.x.discovery.ServiceInstance;
 
-import java.io.Closeable;
-import java.io.IOException;
-import java.net.InetAddress;
-import java.util.Optional;
+import lombok.Builder;
+import lombok.Data;
+import lombok.SneakyThrows;
 
 @Data
 public class DiscoveryClient implements Closeable {
